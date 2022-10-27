d341eaddb96e2b4283780d133dfef06182dd6bb6
Ekaterina_Semenova1 <ekaterina_semenova1@epam.com>
2022-10-27 12:59:44 +0400

    Homework #3 hw4

--- lecture3/hw4.py
+++ lecture3/hw4.py
@@ -11,8 +11,32 @@ assert combinations([1, 2], [3, 4]) == [
     [2, 4],
 ]
 """
+from copy import copy
 from typing import Any, List
+
+one_combination = []
+test_list = []
+start = True
+length: int
 
 
 def combinations(*args: List[Any]) -> List[List]:
-    ...
+    global start
+    global length
+
+    local_list = []
+    if start:
+        length = len(args)
+        start = False
+    for i in range(len(args)):
+        for i_n in range(len(args[i])):
+            one_combination.append(args[i][i_n])
+            combinations(*args[i + 1:])
+            if len(one_combination) == length:
+                test_list.append(copy(one_combination))
+            one_combination.remove(args[i][i_n])
+        if i == length - 1:
+            local_list = copy(test_list)
+            test_list.clear()
+            start = True
+    return local_list
